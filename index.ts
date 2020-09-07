ddbacb405ccbdf7451c0a2642b7e45c59639d3c6
Charlie <itscharlieliu@gmail.com>
2020-09-07 13:49:52 -0700

    Add route for users

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express from "express"
+
 const app = express();
 const PORT = 8000;
 
 app.get('/', (req,res) => res.send('Express + TypeScript Server'));
+
+app.get("/users", (request, response) => response.send("users"));
 
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
